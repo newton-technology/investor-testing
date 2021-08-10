108f76a78627a901a9b6aeafd84f663e58ad1f3a
akubara <akubaradev@gmail.com>
2021-08-10 12:01:19 +0300

    Init

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: false,
+  tabWidth: 4,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+};
