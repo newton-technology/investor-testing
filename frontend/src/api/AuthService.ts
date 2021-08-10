842c2e4cddca33a27e6eab2bd200c2cf7fe2da2a
akubara <akubaradev@gmail.com>
2021-08-10 17:20:26 +0300

    Init authorization structure

---
+++ frontend/src/api/AuthService.ts
@@ -0,0 +1,19 @@
+class AuthService {
+    public Init() {
+        return this;
+    }
+
+    public Login() {
+        return this;
+    }
+
+    public Logout() {
+        return this;
+    }
+
+    public RefreshToken() {
+        return this;
+    }
+}
+
+export default new AuthService();
---
+++ frontend/src/pages/Authorization.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Authorization: React.FC = () => {
+    return (
+        <form>
+            <input type='email' />
+            <input type='password' />
+        </form>
+    );
+};
+
+export default Authorization;
