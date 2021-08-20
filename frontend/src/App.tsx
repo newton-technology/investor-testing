7e470cb4d9930a4dc64b503276aec36e224eee18
oxananana <oxana.khazhieva@gmail.com>
2021-08-20 20:20:23 +0300

    Изменение в App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,48 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import {ThemeProvider} from 'styled-components';
 
 import {CategoryList} from './pages/category_list/CategoryList';
 import {CategoryTest} from './pages/category_test/CategoryTest';
+import {AuthorizationPage} from './pages/AuthorizationPage';
 import {Layout} from './components/Layout';
 import {theme} from './theme/theme';
 import {GlobalStyle} from './theme/GlobalStyle';
 import {useAuthorization} from './hooks/useAuthorization';
-import {AuthorizationPage} from './pages/AuthorizationPage';
 import './api/AuthService';
 
 const App: React.FC = () => {
-    useAuthorization();
+    const {isAuthenticated} = useAuthorization();
 
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
-            <Layout>
+            {isAuthenticated ? (
+                <Layout>
+                    <Switch>
+                        <Route path='/' exact>
+                            <Redirect to='/tests' />
+                        </Route>
+                        <Route path='/tests' exact>
+                            <CategoryList />
+                        </Route>
+                        <Route path='/tests/:id' exact>
+                            <CategoryTest />
+                        </Route>
+                    </Switch>
+                </Layout>
+            ) : (
                 <Switch>
-                    <Route path='/' component={AuthorizationPage} exact />
-                    <Route path='/tests' exact>
-                        <CategoryList />
+                    <Route path='/' exact>
+                        <AuthorizationPage />
                     </Route>
-                    <Route path='/tests/:id' exact>
-                        <CategoryTest />
+                    <Route path='*'>
+                        <Redirect to='/' />
                     </Route>
                 </Switch>
-            </Layout>
+            )}
         </ThemeProvider>
     );
-}
+};
 
 export default App;
