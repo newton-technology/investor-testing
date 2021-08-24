e3d509a9b47382fe71de04beefb8cf0ba944d02d
oxananana <oxana.khazhieva@gmail.com>
2021-08-24 17:46:55 +0300

    wip

--- frontend/src/api/CategoryTestApi.ts
+++ frontend/src/api/CategoryTestApi.ts
@@ -1,14 +1,24 @@
 import axiosWithToken from './axios';
+import {ITest} from '../pages/category_test/CategoryTest';
+
+export interface IResponseError {
+    code: string;
+    message: string;
+}
 
 export const CategoryTestApi = {
     getTest(categoryId: string) {
-        return axiosWithToken.post(`${process.env.REACT_APP_API_URL}/tests`, {categoryId}).then((response) => {
-            return response.data;
-        });
-    },
-    checkTest(testId: string, answers: number[]) {
         return axiosWithToken
-            .patch(`${process.env.REACT_APP_API_URL}/tests/${testId}/answers`, {answers})
+            .post<ITest>(`${process.env.REACT_APP_API_URL}/tests`, {
+                categoryId,
+            })
+            .then((response) => {
+                return response.data;
+            });
+    },
+    checkTest(testId: number, answers: number[]) {
+        return axiosWithToken
+            .patch<ITest>(`${process.env.REACT_APP_API_URL}/tests/${testId}/answers`, {answers})
             .then((response) => {
                 return response.data;
             });
