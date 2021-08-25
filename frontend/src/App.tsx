826ad964d22e4081d052afd1cdc922ad23f208d5
akubara <akubaradev@gmail.com>
2021-08-25 22:05:24 +0300

    WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import {ThemeProvider} from 'styled-components';
 
+import {Layout} from './components/Layout';
+import {useAuthorization} from './hooks/useAuthorization';
+import {AuthorizationPage} from './pages/authorization/AuthorizationPage';
 import {CategoryList} from './pages/category_list/CategoryList';
 import {CategoryTest} from './pages/category_test/CategoryTest';
-import {AuthorizationPage} from './pages/AuthorizationPage';
-import {Layout} from './components/Layout';
-import {theme} from './theme/theme';
 import {GlobalStyle} from './theme/GlobalStyle';
-import {useAuthorization} from './hooks/useAuthorization';
+import {theme} from './theme/theme';
 import './api/AuthService';
 
 const App: React.FC = () => {
