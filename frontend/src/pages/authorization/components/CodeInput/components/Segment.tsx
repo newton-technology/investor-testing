2d5cfc9ff4cd32a897e8b4007d814d7c728afd55
akubara <akubaradev@gmail.com>
2021-08-27 09:36:08 +0300

    Refactor codeinput

--- frontend/src/pages/authorization/components/CodeInput/components/Segment.tsx
+++ frontend/src/pages/authorization/components/CodeInput/components/Segment.tsx
@@ -6,21 +6,21 @@ interface IProps {
     error?: boolean;
 }
 
+export const Segment: React.FC<IProps> = ({children, error}) => <Container error={error}>{children}</Container>;
+
 const Container = styled.div<{error?: boolean}>`
     align-items: center;
     background-color: #ffffff;
-    border: 1px solid ${(props) => (props.error ? '#CD003E' : '#B8BDCE')};
-    border-radius: 5px;
+    border: 1px solid ${(props) => (props.error ? '#E30B17' : '#C4C8DB')};
+    border-radius: 4px;
     box-sizing: border-box;
-    color: ${(props) => (props.error ? '#CD003E' : '#222436')};
+    color: ${(props) => (props.error ? '#E30B17' : '#3A3463')};
     display: flex;
-    font-size: 24px;
+    font-size: 42px;
     font-weight: 500;
-    height: 56px;
+    height: 52px;
     justify-content: center;
     line-height: 28px;
     text-align: center;
-    width: 45px;
+    width: 42px;
 `;
-
-export const Segment: React.FC<IProps> = ({children, error}) => <Container error={error}>{children}</Container>;
