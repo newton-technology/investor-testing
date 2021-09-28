96d542f83d7bc9f5c56571c3241ee3ea057ab710
kolchurin <kolchurin@nwtn-group.com>
2021-09-28 13:30:31 +0500

    Fix

--- install/import-data.sh
+++ install/import-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Импорт данных в БД тестирования"
+echo "Импорт данных в БД тестирования инвесторов"
 
 echo "importing categories"
 docker exec backend /opt/remi/php74/root/bin/php /var/www/projects/php/investor_testing/artisan import categories /var/www/projects/php/investor_testing/resources/demodata/categories.csv
@@ -8,7 +8,7 @@ docker exec backend /opt/remi/php74/root/bin/php /var/www/projects/php/investor_
 echo "importing questions"
 docker exec backend /opt/remi/php74/root/bin/php /var/www/projects/php/investor_testing/artisan import questions /var/www/projects/php/investor_testing/resources/demodata/questions.csv
 
-echo "import answers"
+echo "importing answers"
 docker exec backend /opt/remi/php74/root/bin/php /var/www/projects/php/investor_testing/artisan import answers /var/www/projects/php/investor_testing/resources/demodata/answers.csv 
 
 echo "Импорт завершен"
