8d8e1b8d1f5784063493bd562c128338203dfcbd
Pavel Elfimov <elfimov@nwtn-group.com>
2021-10-21 19:39:04 +0700

    Rename hook useTable to useTablePage

--- frontend/src/pages/admin/components/hooks/useTablePage.ts
+++ frontend/src/pages/admin/components/hooks/useTablePage.ts
@@ -11,23 +11,26 @@ interface ITablePage {
 export const useTablePage = (totalPages: number): ITablePage => {
     const {searchParams, onChangeSearch} = useTableHistory();
     const numberOfPage = Number(searchParams.get(Search.PAGE));
-    const currentPage = (): number => {
+    const currentPage = useCallback((): number => {
         if (numberOfPage > 0 && numberOfPage <= totalPages) {
             return numberOfPage;
         }
         return 1;
-    };
+    }, [numberOfPage, totalPages]);
 
     const [page, setPage] = useState<TPage>(1);
 
     useEffect(() => {
         setPage(currentPage());
-    }, [currentPage()]);
+    }, [currentPage]);
 
-    const onChangePage = useCallback((nextPage: TPage) => {
-        setPage(nextPage);
-        onChangeSearch(Search.PAGE, String(nextPage));
-    }, []);
+    const onChangePage = useCallback(
+        (nextPage: TPage) => {
+            setPage(nextPage);
+            onChangeSearch(Search.PAGE, String(nextPage));
+        },
+        [onChangeSearch],
+    );
 
     return {
         page: page,
