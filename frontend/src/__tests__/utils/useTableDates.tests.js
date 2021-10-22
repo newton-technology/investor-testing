318ef3d57025fa2473bc48f18fd2d06e74d8f20c
Pavel Elfimov <elfimov@nwtn-group.com>
2021-10-22 12:19:16 +0700

    Fix useTableDatesTest

---
+++ frontend/src/__tests__/utils/useTableDates.tests.js
@@ -0,0 +1,40 @@
+import {fireEvent, screen} from '@testing-library/react';
+
+import {renderInRouter} from '../../hoks/test/renderInRouter';
+import {history} from '../../history/history';
+import {renderHook, act} from '@testing-library/react-hooks';
+import {Router} from 'react-router-dom';
+import React from 'react';
+import {useTableDates} from '../../pages/admin/components/hooks/useTableDates';
+
+const ReactMock = () => {
+    const {datesValue, onDateChange} = useTableDates();
+
+    return (
+        <>
+            <input data-testid='dateStart' value={datesValue.dateStart} name='dateStart' onChange={onDateChange} />
+            <input data-testid='dateEnd' value={datesValue.dateEnd} name='dateEnd' onChange={onDateChange} />;
+        </>
+    );
+};
+
+const render = () => renderInRouter(ReactMock);
+
+describe('Set dates using the useTableDates hook', () => {
+    render();
+    const inputStart = screen.getByTestId('dateStart');
+    const inputEnd = screen.getByTestId('dateEnd');
+
+    it('Hook returned a start date similar to the input data.', () => {
+        act(() => {
+            fireEvent.change(inputStart, {target: {value: '2021-10-09'}});
+        });
+
+        expect(inputStart.value).toBe('2021-10-09');
+    });
+
+    it('Hook returned a end date similar to the input data.', () => {
+        fireEvent.change(inputEnd, {target: {value: '2021-10-08'}});
+        expect(inputEnd.value).toBe('2021-10-08');
+    });
+});
