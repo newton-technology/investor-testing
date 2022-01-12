a6969c39d53bc02491cb91f59e7f767bf253601e
Александр Козлов <akozlov@Aleksandrs-MBP.Dlink>
2022-01-12 12:24:25 +0300

    Добавлен красный цвет для выделения в палитру цветов

--- frontend/src/styled.d.ts
+++ frontend/src/styled.d.ts
@@ -7,6 +7,7 @@ declare module 'styled-components' {
             primary: string;
             secondary: string;
             error: string;
+            featured: string;
             bg: {
                 primary: string;
                 secondary: string;
